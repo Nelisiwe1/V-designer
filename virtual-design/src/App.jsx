957620fa1a2f3548300b7c709a7ee7a3e0d00ac9
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-03-02 01:18:23 +0200

    second commit

--- virtual-design/src/App.jsx
+++ virtual-design/src/App.jsx
@@ -1,11 +1,24 @@
-// src/App.js
 import React from 'react';
 import Navbar from './Navbar';
+import './index.css';
+import Home from './components/Home';
+import About from './components/About';
+import Blogs from './components/Blogs';
+import ContactUs from './components/ContactUs';
+import Services from './components/Services';
+import Projects from './components/Projects';
+ 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
+      <About />
+      <Blogs />
+      <ContactUs />
+      <Projects />
+      <Services />
      
     </div>
   );
---
+++ virtual-design/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div className='Home'>
+
+        <h1>TSN interiorDesign </h1>
+        <h3>
+        "Illuminate Your Vision: Innovative Designs, Reflecting You with Our Expertise."</h3>
+</div>
+   
+  )
+}
+
+export default Home
