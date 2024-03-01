e2e768d3c773ca876ab75eae06d35aa437d6ad84
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-03-01 02:41:29 +0200

    first

---
+++ virtual-design/src/Navbar.jsx
@@ -0,0 +1,19 @@
+// src/components/Navbar.js
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Blogs</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
